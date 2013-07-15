382429ed0afd1e175f172c9503690ba539426cd2
luciepoudoulec <lucie.poudoulec@gmail.com>
2013-07-15 17:24:01 +0200

    updated web.js again

--- web.js
+++ web.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var fs = require('fs');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-fs.readFile('./Index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    response = data;
-});
 
+var contents =  fs.readFile('index.html','utf8',function(err, data) {
+     if(err) {
+         response.send('unable to load the requested file');
+        }
+    else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+response.write(data);
+}
+response.end();
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
